e3ea2153554db34a87574ff6f13b7b90c54d51a2
Andy Bingham <andy.bingham@agrantec.com>
2021-03-02 16:38:55 +0000

    First attempts

--- wiegand.py
+++ wiegand.py
@@ -7,20 +7,28 @@ from machine import Pin
 # set x = 0 for D0 and &ff for D1
 
 
-@rp2.asm_pio(autopull=True, pull_thresh=8)
+@rp2.asm_pio(autopush=True, push_thresh=8)
 def wiegand_bit():
     wrap_target()
-    wait(1, 0)  # wait for the start of a bit
-    wait(0, 0)  # wait for the end of a bit
-    in(x, 1)  # send the bit, x will be 00, or ff
+    wait(0, 1, 0)  # wait for the start of a bit
+    wait(1, 1, 0)  # wait for the end of a bit
+    in_(x, 1)  # send the bit, x will be 00, or ff
     wrap()
 
 
-sm-d0 = rp2.StateMachine(0,wiegand_bit,freq=1000, set_base=Pin(5), set_x=0, in_shiftdir=0)
-sm-d1 = rp2.StateMachine(1,wiegand_bit,freq=1000, set_base=Pin(6), set_x=1, in_shiftdir=0)
+smD0 = rp2.StateMachine(0, wiegand_bit, freq=8_000_000,
+                        set_base=Pin(5), in_shiftdir=0)
+smD0.exec('set(x,0)')
 
-sm-d0.active()
-sm-d1.active()
+smD1 = rp2.StateMachine(1, wiegand_bit, freq=8_000_000,
+                        set_base=Pin(6), in_shiftdir=0)
+smD1.exec('set(x,255)')
 
+smD0.active(1)
+smD1.active(1)
+
+print("Starting")
 while True:
-    print data.append(sm-d0.get())
+    byte = smD0.get()
+    print('Byte')
+    print(byte)
